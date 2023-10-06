fddda7d7ef38f4bf1a99a199909f009ef278831d
unknown <mayankvaidya436@gmail.com>
2023-10-06 17:44:06 +0530

    second  commit

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-console.log("mayab");
+
 var items=document.getElementsByClassName("list-group-item");
 items[2].style.backgroundColor='green';
 for (let index = 0; index < items.length; index++) {
---
+++ five.js
@@ -0,0 +1,7 @@
+var a=document.getElementsByClassName("list-group-item");
+for (let index = 0; index < a.length; index++) {
+   
+   // a[index].style.backgroundColor="red";
+}
+var li=document.getElementsByTagName('li');
+li[4].style.backgroundColor="yellow";
