c35537b36030d2c25c377efbf0d4ede8721ad803
unknown <mayankvaidya436@gmail.com>
2023-10-06 14:59:05 +0530

    first commit

---
+++ class.js
@@ -0,0 +1,7 @@
+console.log("mayab");
+var items=document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor='green';
+for (let index = 0; index < items.length; index++) {
+    items[index].style.fontWeight='bold';
+    
+}
